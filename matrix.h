b89b3ce4ec8fda4a743ac196911a6d69a945b1e7
Jameson Thatcher <jameson@bluescreenofdoom.com>
2017-05-10 00:37:58 +0100

    Added code to satisfy the first batch of tests

--- matrix.h
+++ matrix.h
@@ -1,3 +1,129 @@
 #pragma once
 
+#include <initializer_list>
+#include <type_traits>
+
+template<
+	typename BaseType,
+	int t_w,
+	int t_h,
+	typename DowncastType = BaseType
+> class Matrix {
+public:
+	constexpr Matrix() = default;
+	constexpr Matrix(std::initializer_list<BaseType> list) {
+		auto elem = list.begin();
+		for(auto y = 0u; y < t_h; ++y)
+			for(auto x = 0u; x < t_w && elem != list.end(); ++x, ++elem)
+				data[x][y] = *elem;
+	}
+
+	constexpr BaseType const operator()(unsigned x, unsigned y) const {
+		return data[x][y];
+	}
+
+	constexpr BaseType operator()(unsigned x, unsigned y) {
+		return data[x][y];
+	}
+
+	constexpr Matrix& operator+=(
+		Matrix const& other
+	) {
+		for(auto x = 0u; x < t_w; ++x)
+			for(auto y = 0u; y < t_h; ++y)
+				data[x][y] += other.data[x][y];
+		return *this;
+	}
+
+	constexpr Matrix& operator-=(
+		Matrix const& other
+	) {
+		for(auto x = 0u; x < t_w; ++x)
+			for(auto y = 0u; y < t_h; ++y)
+				data[x][y] -= other.data[x][y];
+		return *this;
+	}
+
+	// Static Constants
+	static constexpr Matrix Zero() {
+		return Matrix{};
+	}
+
+	template<
+		typename DepType = BaseType,
+		typename = std::enable_if_t<t_w == t_h, DepType>
+	>
+	static constexpr Matrix Identity() {
+		auto ret = Matrix{};
+		for(auto i = 0u; i < t_w; ++i)
+			ret(i, i) == BaseType{1};
+		return ret;
+	}
+
+private:
+	BaseType data[t_w][t_h]{};
+};
+
+// unary operators
+template<
+	typename BaseType,
+	int t_w,
+	int t_h,
+	typename DowncastType = BaseType
+>
+constexpr Matrix<BaseType, t_w, t_h, DowncastType> operator- (
+	Matrix<BaseType, t_w, t_h, DowncastType> const& op
+) {
+	return Matrix<BaseType, t_w, t_h, DowncastType>::Zero() - op;
+}
+
+// binary operators
+template<
+	typename BaseType,
+	int t_w,
+	int t_h,
+	typename DowncastType = BaseType
+>
+constexpr Matrix<BaseType, t_w, t_h, DowncastType> operator+ (
+	Matrix<BaseType, t_w, t_h, DowncastType> const& left,
+	Matrix<BaseType, t_w, t_h, DowncastType> const& right
+) {
+	auto ret = left;
+	ret += right;
+	return ret;
+}
+
+template<
+	typename BaseType,
+	int t_w,
+	int t_h,
+	typename DowncastType = BaseType
+>
+constexpr Matrix<BaseType, t_w, t_h, DowncastType> operator- (
+	Matrix<BaseType, t_w, t_h, DowncastType> const& left,
+	Matrix<BaseType, t_w, t_h, DowncastType> const& right
+) {
+	auto ret = left;
+	ret -= right;
+	return ret;
+}
+
+//Comparison operators
+
+template<
+	typename BaseType,
+	int t_w,
+	int t_h,
+	typename DowncastType = BaseType
+>
+constexpr bool operator== (
+	Matrix<BaseType, t_w, t_h, DowncastType> const& left,
+	Matrix<BaseType, t_w, t_h, DowncastType> const& right
+) {
+	for(auto x = 0u; x < t_w; ++x)
+		for(auto y = 0u; y < t_h; ++y)
+			if(left(x, y) != right(x, y))
+				return false;
+	return true;
+}
 
--- tests/main.cpp
+++ tests/main.cpp
@@ -9,6 +9,26 @@
 
 using namespace std;
 
+Tee_Test(test_matrix_constants) {
+	Tee_SubTest(test_matrix_identity) {
+		auto i = Matrix<float, 4, 4>::Identity();
+
+		for(auto x = 0u; x < 4; ++x)
+			for(auto y = 0u; y < 4; ++y)
+				assert(
+					i(x,y) == (x == y ? 1.0f : 0.0f)
+				);
+	}
+
+	Tee_SubTest(test_matrix_zero) {
+		auto z = Matrix<float, 4, 4>::Zero();
+		for(auto x = 0u; x < 4; ++x)
+			for(auto y = 0u; y < 4; ++y)
+				assert(z(x, y) == 0.0f);
+	}
+}
+
+
 Tee_Test(test_basic_matrix_addition) {
 	auto m = Matrix<float, 2, 2>{1, 2, 3, 4};
 	auto m2 = Matrix<float, 2, 2>{5, 6, 7, 8};
@@ -17,7 +37,7 @@ Tee_Test(test_basic_matrix_addition) {
 	Tee_SubTest(test_adding_2x2) {
 		auto r = m + m2;
 		assert(
-			r(0,0) == 6
+			r(0, 0) == 6
 			&& r(1, 0) == 8
 			&& r(0, 1) == 10
 			&& r(1, 1) == 12
