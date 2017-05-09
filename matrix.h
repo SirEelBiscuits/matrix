173a1a812687629af8f111246c51da1d922069fa
Jameson Thatcher <jameson@bluescreenofdoom.com>
2017-05-09 23:31:58 +0100

    Fixed comments to fit in a reasonable screen width

--- matrix.h
+++ matrix.h
@@ -43,20 +43,23 @@ namespace TypeMagic {
 /*
  * Generic matrix.
  *
- * Should work with any type specified for BaseType, so long as it has basic overloads.
- * Types which have 'interesting' mutations multiplication and division _should_ work
- * I am not currently supporting mutation under addition because wtff.
+ * Should work with any type specified for BaseType, so long as it has basic
+ * overloads.  Types which have 'interesting' mutations multiplication and
+ * division _should_ work I am not currently supporting mutation under
+ * addition because wtff.
  *
- * DownCastType is for intermediate operations where types might be accumulating type information.
- * This isn't permissable in C++ usually, but the matrix will maintain return type validity, and
- * just use this value internally. If it compiles with the default, the default is fine!
+ * DownCastType is for intermediate operations where types might be
+ * accumulating type information.  This isn't permissable in C++ usually,
+ * but the matrix will maintain return type validity, and just use this
+ * value internally. If it compiles with the default, the default is fine!
  *
- * example for when DownCastType is needed:
- *   BaseType = T<int m_pow> where m_pow is the exponent of the meters unit. Multiplying two together
- *   will result in summing their m_pow to maintain the unit validity. If these use float as a storage
- *   type, and can be cast to and from, then DownCastType should be float. If there is no such type,
- *   make one as a helper type to use here. The DownCastType must have its operators return the same
- *   type, always.
+ * example for when DownCastType is needed: BaseType = T<int m_pow> where
+ * m_pow is the exponent of the meters unit. Multiplying two together will
+ * result in summing their m_pow to maintain the unit validity. If these use
+ * float as a storage type, and can be cast to and from, then DownCastType
+ * should be float. If there is no such type, make one as a helper type to
+ * use here. The DownCastType must have its operators return the same type,
+ * always.
  */
 template<typename BaseType, unsigned t_w, unsigned t_h, typename DownCastType = BaseType>
 class Matrix {
--- tests/main.cpp
+++ tests/main.cpp
@@ -9,6 +9,38 @@
 
 using namespace std;
 
+Tee_Test(test_basic_matrix_addition) {
+	auto m = Matrix<float, 2, 2>{1, 2, 3, 4};
+	auto m2 = Matrix<float, 2, 2>{5, 6, 7, 8};
+	auto m3 = Matrix<float, 2, 2>{1, 3, 6, 7};
+
+	Tee_SubTest(test_adding_2x2) {
+		auto r = m + m2;
+		assert(
+			r(0,0) == 6
+			&& r(1, 0) == 8
+			&& r(0, 1) == 10
+			&& r(1, 1) == 12
+		);
+	}
+
+	Tee_SubTest(test_addition_is_commutative) {
+		assert(m + m2 == m2 + m);
+	}
+
+	Tee_SubTest(test_addition_is_associative) {
+		assert((m + m2) + m3 == m + (m2 + m3));
+	}
+
+	Tee_SubTest(test_negation_is_involuting) {
+		assert(-(-m) == m);
+	}
+
+	Tee_SubTest(test_subtraction_negates_addition) {
+		assert(m2 + m - m2 == m);
+	}
+}
+
 int main() {
 	int successes;
 	vector<string> fails;
